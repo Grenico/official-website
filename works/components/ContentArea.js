7e7cbc2b5052aaba8580aa5ef9d6da70df41685d
Grenico <2188342948@qq.com>
2026-01-11 16:15:07 +0800

    修改首页图片加载方式，修改text回到顶部按钮图标显示问题，添加加载动画

--- works/components/ContentArea.js
+++ works/components/ContentArea.js
@@ -6,8 +6,10 @@ export default {
     <div class="content-area">
     <p v-if="!showIframe" class="works">作品一览</p>
     <all-panels v-if="!showIframe" @panel-selected="handlePanelEvent" ></all-panels>
-    <iframe v-if="showIframe" :src="iframeSrc" style="width:100%;height:100vh;border:none;" frameborder="0" allow-scripts allow-same-origin ></iframe>
+    <div class="loadingAnimation" v-loading="loading" element-loading-background="#d0dfee">
+    <iframe v-if="showIframe"  @load="onIframeLoaded" :src="iframeSrc" style="width:100%;height:100vh;border:none;" frameborder="0" allow-scripts allow-same-origin ></iframe>
   </div>
+    </div>
     `,
   props: {
     showIframe: {
@@ -17,6 +19,27 @@ export default {
     iframeSrc: {
       type: String,
       default: "",
+    },
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  watch: {
+    // 监听 iframeSrc 变化，开始加载
+    iframeSrc(newVal) {
+      if (newVal) {
+        this.loading = true;
+      }
+    },
+    // 监听 showIframe 变化
+    showIframe(newVal) {
+      if (newVal) {
+        this.loading = true;
+      } else {
+        this.loading = false;
+      }
     },
   },
   mounted() {
@@ -36,6 +59,9 @@ export default {
       if (event.data.type === "scroll") {
         this.$emit("scroll", { scrollTop: event.data.scrollTop });
       }
+    },
+    onIframeLoaded() {
+      this.loading = false; //加载完成后关闭加载动画
     },
   },
 };
